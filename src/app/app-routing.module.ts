36c890ad36dfe597718dc30294adad14b6a164d3
ConnorJWalker <connorjwalker32@hotmail.co.uk>
2019-06-10 03:48:20 +0100

    removed boilerplate code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginViewComponent } from './views/login-view/login-view.component'
+
+const routes: Routes = [
+  { path: '', component: LoginViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
