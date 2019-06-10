afb0eaac872b28570f6cddb572e56303cd076d2d
ConnorJWalker <connorjwalker32@hotmail.co.uk>
2019-06-10 03:29:12 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginViewComponent } from './views/login-view/login-view.component';
+import { LoginFromComponent } from './views/login-view/login-from/login-from.component';
+import { SignupFromComponent } from './views/login-view/signup-from/signup-from.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginViewComponent,
+    LoginFromComponent,
+    SignupFromComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/views/login-view/signup-from/signup-from.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-from',
+  templateUrl: './signup-from.component.html',
+  styleUrls: ['../login-view.component.css']
+})
+export class SignupFromComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
